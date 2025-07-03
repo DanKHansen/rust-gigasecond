2a7881782e03c8e52da57c51530a55c98a425078
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-03 15:30:29 +0200

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use time::{Duration, PrimitiveDateTime as DateTime};
 
-// Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start + Duration::seconds(1e9 as i64)
+    start + Duration::seconds(1_000_000_000)
 }
